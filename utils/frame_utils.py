a6e203142dd02192fb8184435eabac7504e543b4
mirror_HJ <MG1933022@smail.nju.edu.cn>
2019-10-23 22:42:11 +0800

    Can run main.py! Replace scipy by imageio. 2019/10/23

--- utils/frame_utils.py
+++ utils/frame_utils.py
@@ -1,12 +1,12 @@
 import numpy as np
 from os.path import *
-from scipy.misc import imread
+# from scipy.misc import imread
 from . import flow_utils 
-
+import imageio
 def read_gen(file_name):
     ext = splitext(file_name)[-1]
     if ext == '.png' or ext == '.jpeg' or ext == '.ppm' or ext == '.jpg':
-        im = imread(file_name)
+        im = imageio.imread(file_name)
         if im.shape[2] > 3:
             return im[:,:,:3]
         else:
